c8bd9859cda3daa4b4bdfc5fcbd737ff8fc77f1e
Kushal Vala <kushalvala@Kushals-MacBook-Pro.local>
2024-07-21 22:17:28 -0400

    changes in button and message component for mobile layout

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,7 +6,7 @@ const Button = ({ onClick, children, className, disabled }) => {
       onClick={onClick}
       className={`
        text-white drop-shadow-md disabled:opacity-60 disabled:cursor-not-allowed
-       px-4 py-3 rounded ${className}`}
+       px-4 max-sm:py-3 py-2 rounded ${className}`}
       disabled={disabled}>
       {children}
     </button>
